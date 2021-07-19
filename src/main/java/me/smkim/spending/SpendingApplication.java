3ae14bad5a939ec034669434808b8b5175555244
sangminK <smkim2339@gmail.com>
2021-07-19 23:35:50 +0900

    init

---
+++ src/main/java/me/smkim/spending/SpendingApplication.java
@@ -0,0 +1,13 @@
+package me.smkim.spending;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpendingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/smkim/spending/SpendingApplicationTests.java
@@ -0,0 +1,13 @@
+package me.smkim.spending;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpendingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
